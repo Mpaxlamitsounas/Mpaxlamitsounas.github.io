2be8311bacef32809b163801f5a8e9da47ff073e
mpaxlamitsounas <worldstudy123@gmail.com>
2024-10-04 19:30:50 +0300

    i PRAY this looks fine on mobile now

--- js/scale_visibility.js
+++ js/scale_visibility.js
@@ -3,4 +3,5 @@ var scale = document.getElementById("scale")
 function toggleVisibility() {
     scale.style.backgroundColor = "rgba(128, 128, 128, 0.8)"
     scale.style.border = "2px solid black"
+    scale.style.textShadow = "1px 1px 3px black"
 }
