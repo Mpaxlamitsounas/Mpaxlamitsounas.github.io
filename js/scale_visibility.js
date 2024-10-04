712a7142761d33ecf16a969d39b26c479b83650d
mpaxlamitsounas <worldstudy123@gmail.com>
2024-10-04 16:39:09 +0300

    Add accessibility button

---
+++ js/scale_visibility.js
@@ -0,0 +1,6 @@
+var scale = document.getElementById("scale")
+
+function toggleVisibility() {
+    scale.style.backgroundColor = "rgba(128, 128, 128, 0.8)"
+    scale.style.border = "2px solid black"
+}
